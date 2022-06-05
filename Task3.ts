a1d1d0cfa49eb91be5cbdbcf0e11a4ac09586e5a
chris-ian-jones <chrisjones@Chriss-MacBook-Pro.local>
2022-06-05 15:10:43 +0100

    Task3: initial commit

---
+++ Task3.ts
@@ -0,0 +1,12 @@
+const inputArr = [ 2, 3, 10, 11, 12, 22, 297 ];
+
+function bazFizzBuzz(input) {
+  const answerArr = []
+
+  input.forEach(element => {
+    answerArr.push(element)
+  })
+  return answerArr
+}
+
+bazFizzBuzz(inputArr)
